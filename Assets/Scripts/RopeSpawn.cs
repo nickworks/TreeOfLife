82025ada4d938fbc6fa1e9fa07653247817d48d5
STUDENT_AD\leathes1 <xgigaroidx@gmail.com>
2017-10-05 16:55:30 -0400

    Started adding ropestuff.

---
+++ Assets/Scripts/RopeSpawn.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSpawn : MonoBehaviour {
+    public GameObject ropePiece;
+    private Rigidbody2D playerBody;
+    private List<GameObject> linksList = new List<GameObject>();
+    public int numberOfSegments = 12;
+    public int totalLength = 4;
+
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.collider.tag == "Player")
+        {
+            playerBody = collision.rigidbody;
+            LinkRope();
+        }
+    }
+
+    /// <summary>
+    /// This creates the chain/rope and connects it from the player to the hook.
+    /// </summary>
+	void LinkRope () {
+        float desiredLinkLength = totalLength / numberOfSegments;
+        Rigidbody2D lastLink = null;
+        // The position of the hook.
+        Vector2 pos = transform.position;
+        // The position of the player.
+        Vector2 playerPos = playerBody.transform.position;
+        float linkDir = Mathf.Atan2(playerPos.y - pos.y, playerPos.x - pos.x);
+        float realDistance = Vector2.Distance(pos, playerPos);
+        float realLinkLength = realDistance / numberOfSegments;
+        for (int i = 0; i < numberOfSegments; i++) {
+            GameObject piece = Instantiate(ropePiece);
+            linksList.Add(piece);
+            Vector2 myPos = playerPos;
+            myPos.x += Mathf.Cos(linkDir) * realLinkLength * i;
+            myPos.y += Mathf.Sin(linkDir) * realLinkLength * i;
+            piece.transform.position = myPos;
+            DistanceJoint2D joint = piece.GetComponent<DistanceJoint2D>();
+            joint.distance = desiredLinkLength;
+            if (lastLink == null) {
+                playerBody.bodyType = RigidbodyType2D.Dynamic;
+                joint.connectedBody = playerBody;
+            }
+            else {
+                joint.connectedBody = lastLink;
+            }
+            lastLink = piece.GetComponent<Rigidbody2D>();
+        }
+	}
+	
+    /// <summary>
+    /// Should be called when the player lets go of the rope.
+    /// </summary>
+	void UnlinkRope () {
+        playerBody.bodyType = RigidbodyType2D.Kinematic;
+        for (int i = linksList.Count; i >= 0; i--) {
+            GameObject obj = linksList[i];
+            linksList.RemoveAt(i);
+            Destroy(obj);
+        }
+	}
+}
