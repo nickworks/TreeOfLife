39c7376217c0ed50c9074394cf434333cb8f008e
Unknown <drummond.jacob13@gmail.com>
2017-10-19 12:47:09 -0400

    made a functioning kupa

---
+++ Assets/Scripts/EnemyDasher.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PawnAABB))]
+public class EnemyDasher : MonoBehaviour
+{
+    public bool inRange = false;
+    public Vector3 targetPos;
+   
+   // Vector3 currentPos;
+    public float enemeyImpulse;
+    private Vector3 velocity = new Vector3();
+    private PawnAABB pawn;
+    private float gravity = 10;
+     private bool isGrounded = false;
+    public bool canDash = true;
+    public bool isHit = false;
+    private bool moveRight = true;
+   private float moveCount;
+    public float moveCountReset = 10;
+    public float speed = 10;
+
+
+
+    void Start()
+    {
+        pawn = GetComponent<PawnAABB>();
+        moveCount = moveCountReset;
+       
+    }
+
+   
+    void Update()
+    {
+        Move();
+        DecelerateX(20);
+        DoCollisions();       
+        if (inRange)
+        {                 
+            Dash();
+        }
+        float gravityScale = 2;
+        velocity.y -= gravity * Time.deltaTime * gravityScale;
+    }
+
+
+    private void Dash()
+    {
+        if (isHit)
+        {
+            if (canDash)
+            {
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    velocity = DirectionalDash.rot * enemeyImpulse;
+                    canDash = false;
+
+                }
+            }
+        }
+    }
+    private void Move()
+    {
+        if (!isHit)
+        {
+            if (moveRight)
+            {
+                velocity.x += speed * Time.deltaTime;
+                moveCount -= Time.deltaTime;
+                if (moveCount <= 0)
+                {
+                    moveRight = false;
+                    moveCount = moveCountReset;
+
+                }
+
+            } else {
+                velocity.x += -speed * Time.deltaTime;
+                moveCount -= Time.deltaTime;
+                if (moveCount <= 0)
+                {
+                    moveRight = true;
+                    moveCount = moveCountReset;
+
+                }
+            }
+        }
+    }
+    private void DecelerateX(float amount)
+    {
+        // slow down the player
+        if (velocity.x > 0) // moving right...
+        {
+            AccelerateX(-amount);
+            if (velocity.x < 0) velocity.x = 0;
+        }
+        if (velocity.x < 0) // moving left...
+        {
+            AccelerateX(amount);
+            if (velocity.x > 0) velocity.x = 0;
+        }
+    }
+    private void AccelerateX(float amount)
+    {
+        velocity.x += amount * Time.deltaTime;
+    }
+    private void DoCollisions()
+    {
+        PawnAABB.CollisionResults results = pawn.Move(velocity * Time.deltaTime);
+        if (results.hitTop || results.hitBottom) velocity.y = 0;
+        if (results.hitLeft || results.hitRight) velocity.x = 0;
+        isGrounded = results.hitBottom || results.ascendSlope;
+        transform.position += results.distance;
+    }
+}
---
+++ Assets/Scripts/ThrowTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().inRange = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().targetPos = collision.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().inRange = false;
+        }
+
+    }
+}
