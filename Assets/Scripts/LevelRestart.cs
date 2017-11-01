acff19c95552c9496a67843f637b7160a29854c2
Cameron Blaine <camerong124@gmail.com>
2017-11-01 18:28:47 -0400

    Feature Level Restart Added
    Level Restart development tool added.

---
+++ Assets/Scripts/LevelRestart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Level Restart is a Modular Tool that allows developers to restart the level or as a final game ending.
+/// This is a modular tool
+/// Use : Please use the string tag to customize what level you wish to restart
+/// requires the trigger box ticked in order to work
+/// Along with a rigidbody2d/boxcollider
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+public class LevelRestart : MonoBehaviour {
+    /// <summary>
+    /// Level is the string attached to change the behavior of what scene to load up
+    /// </summary>
+    public string Level;
+
+    /// <summary>
+    /// OnTriggerEnter2d 
+    /// Checks to see if the collider is a player.
+    /// If it is true the scene manager loads a scene
+    /// This is meant for debugging or loading a new level
+    /// </summary>
+    /// <param name="collider"></param>
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Level);
+
+        }
+
+    }
+}
