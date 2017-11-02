88a2667779e63d06b702630e04cba574183a7163
Unknown <smithja@ferris.edu>
2017-11-02 14:42:45 -0400

    You need to comment boi
    I mean yeah you are ready to merge you just need to comment. That's it. Also you should really lock the z axis or just use a vector 2 for moving the platform.

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Player;
 
+
+//FIXME: Comment all variables and classes
 public class PlatformController : MonoBehaviour
 {
 
@@ -10,26 +12,32 @@ public class PlatformController : MonoBehaviour
     /// Which layers this object can collide with.
     /// </summary>
     public LayerMask playerMask;
-
+    //FIXME: Comment all variables and classes
+    //FIXME: Make a vector 2 so nobody tinkers with the z axis but this will probably get reworked cause of the whole pathing thing
     public Vector3 move;
-
+    //FIXME: Comment all variables and classes
     public PlayerController player;
-
+    //FIXME: Comment all variables and classes
     List<PassengerMovement> passengerMovement;
-
+    //FIXME: Comment all variables and classes
     public const float skinWidth = .015f;
+    //FIXME: Comment all variables and classes
     public int horizontalRayCount = 3;
+    //FIXME: Comment all variables and classes
     public int verticalRayCount = 6;
-
+    //FIXME: Comment all variables and classes
     public float horizontalRaySpacing;
+    //FIXME: Comment all variables and classes
     public float verticalRaySpacing;
-
+    //FIXME: Comment all variables and classes
     BoxCollider2D collider;
+    //FIXME: Comment all variables and classes
     public RaycastOrigins raycastOrigins;
 
-    // Use this for initialization
+    //FIXME: Comment what you are doing here
     public void Start()
     {
+        //FIXME: Comment what you are doing here
         collider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
         player = FindObjectOfType<PlayerController>();
@@ -38,53 +46,71 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //FIXME: Comment what you are doing here
         UpdateRaycastOrigins();
 
+        //FIXME: Comment what you are doing here
         Vector3 velocity = move * Time.deltaTime;
-
+        //FIXME: Comment what you are doing here
         MovePlayer(velocity);
-
+        //FIXME: Comment what you are doing here
         //transform.Translate(velocity);
         MovePassangers(true);
+        //FIXME: Comment what you are doing here
         transform.position += velocity;
+        //FIXME: Comment what you are doing here
         //transform.position = move;
         MovePassangers(false);
     }
 
+    //FIXME: Comment what you are doing here
     void MovePassangers(bool beforeMovePlatform)
     {
-        foreach(PassengerMovement passenger in passengerMovement)
+        //FIXME: What is this for each loop doing
+        foreach (PassengerMovement passenger in passengerMovement)
         {
-            if(passenger.moveBeforePlatform == beforeMovePlatform)
+            //FIXME: What is this if statement doing
+            if (passenger.moveBeforePlatform == beforeMovePlatform)
             {
+                //FIXME: Comment what you are doing here
                 PawnAABB.CollisionResults results = passenger.transform.GetComponent<PlayerController>().pawn.Move(passenger.velocity);
+                //FIXME: Comment what you are doing here
                 passenger.transform.position += results.distance;
             }
         }
     }
 
+
+    //FIXME: Comment what you are doing here
     void MovePlayer(Vector3 velocity)
     {
+        //FIXME: Comment what you are doing here
         HashSet<Transform> movedPlayers = new HashSet<Transform>();
         passengerMovement = new List<PassengerMovement>();
-
+        //FIXME: Comment what you are doing here
         float directionX = Mathf.Sign(velocity.x);
         float directionY = Mathf.Sign(velocity.y);
 
-        // Vertically moving platform
+        //FIXME: What is this if statement doing
         if (velocity.y != 0)
         {
             float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+            //FIXME: What is this for loop doing
             for (int i = 0; i < verticalRayCount; i++)
             {
+                //FIXME: Comment what you are doing here
                 Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                //FIXME: Comment what you are doing here
                 rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                //FIXME: Comment what you are doing here
                 RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, playerMask);
-
+                //FIXME: What is this if statement doing
                 if (hit)
                 {
+                    //FIXME: What is this if statement doing
                     if (!movedPlayers.Contains(hit.transform))
                     {
+                        //FIXME: Comment what you are doing here
                         movedPlayers.Add(hit.transform);
                         float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
                         float pushX = (directionY == 1) ? velocity.x : 0;
@@ -98,19 +124,22 @@ public class PlatformController : MonoBehaviour
             }//end for loop
         }//end if vertical
 
+
+        //FIXME: What is this if statement doing
         //horizontal moving platform
         if (velocity.x != 0)
         {
             float rayLength = Mathf.Abs(velocity.x) + skinWidth;
-
+            //FIXME: What is this for loop doing
             for (int i = 0; i < horizontalRayCount; i++)
             {
                 Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
                 rayOrigin += Vector2.up * (horizontalRaySpacing * i);
                 RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, playerMask);
-
+                //FIXME: What is this if statement doing
                 if (hit)
                 {
+                    //FIXME: What is this if statement doing
                     if (!movedPlayers.Contains(hit.transform))
                     {
                         movedPlayers.Add(hit.transform);
@@ -126,6 +155,8 @@ public class PlatformController : MonoBehaviour
             }//end for loop
         }//end if vertical*/
 
+
+        //FIXME: What is this if statement doing
         // Player on top platform moving horizontally or down
         if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
         {
@@ -134,9 +165,10 @@ public class PlatformController : MonoBehaviour
             {
                 Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
                 RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, playerMask);
-
+                //FIXME: What is this if statement doing
                 if (hit)
                 {
+                    //FIXME: What is this if statement doing
                     if (!movedPlayers.Contains(hit.transform))
                     {
                         movedPlayers.Add(hit.transform);
@@ -152,14 +184,17 @@ public class PlatformController : MonoBehaviour
             }//end for loop
         }//end if vertical
     }
-
+    //FIXME: Comment what you are doing here
     struct PassengerMovement
     {
+
+        //FIXME: What are these variables for
         public Transform transform;
         public Vector3 velocity;
         public bool standingOnPlatform;
         public bool moveBeforePlatform;
 
+        //FIXME: What are you doing here
         public PassengerMovement(Transform t, Vector3 v, bool onPlat, bool beforePlat)
         {
             transform = t;
@@ -168,7 +203,7 @@ public class PlatformController : MonoBehaviour
             moveBeforePlatform = beforePlat;
         }
     }
-
+    //FIXME: Comment what you are doing here
     public void UpdateRaycastOrigins()
     {
         Bounds bounds = collider.bounds;
@@ -185,7 +220,7 @@ public class PlatformController : MonoBehaviour
         Debug.DrawLine(new Vector3(bounds.max.x, bounds.max.y, 0), new Vector3(bounds.min.x, bounds.max.y));
         Debug.DrawLine(new Vector3(bounds.min.x, bounds.max.y, 0), new Vector3(bounds.min.x, bounds.min.y));
     }
-
+    //FIXME: Comment what you are doing here
     public void CalculateRaySpacing()
     {
         Bounds bounds = collider.bounds;
@@ -197,7 +232,7 @@ public class PlatformController : MonoBehaviour
         horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
         verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
     }
-
+    //FIXME: Comment what you are doing here
     public struct RaycastOrigins
     {
         public Vector2 topLeft, topRight;
