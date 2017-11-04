1df306503103a7f47f58b1e6a25fd3e1604e7e21
Cameron Blaine <camerong124@gmail.com>
2017-11-04 14:05:33 -0400

    Version05
    Changes to Level Restart - added a require component for box collider
    (Seems not wanting to work)
    Prison Level01 - moved objects around in general level design.
    
    and Treeforest details were added to the prefab instance
    
    Issue Level restart not working as intended.

--- Assets/Scripts/Environment/LevelRestart.cs
+++ Assets/Scripts/Environment/LevelRestart.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-/// <summary>
-/// Level Restart is a Modular Tool that allows developers to restart the level or as a final game ending.
-/// 
-/// </summary>
-public class LevelRestart : MonoBehaviour {
-
+[RequireComponent(typeof(BoxCollider))]
+///Level Restart
+///
+public class LevelRestart : MonoBehaviour
+{
+    /// <summary>
+    /// Public String of level, where user types in level. //will be changed
+    /// </summary>
     public string Level;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    public void OnTriggerEnter(Collider collider)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /// <summary>
+    /// On Collision with Player new scene is loaded
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
         {
