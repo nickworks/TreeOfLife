7f034f65580e41207db9e4a3b2834850e942e413
Nick <pattison.nick@gmail.com>
2017-11-09 14:17:55 -0500

    EnemyDasher
    Added the unfortunately named EnemyDasher, which defines a throwable enemy.
    
    TODO: rename to ThrowableEnemy (or something similar)
    TODO: decouple this feature from the directional dash feature

---
+++ Assets/Scripts/NPCs/ThrowTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowTrigger : MonoBehaviour {   
+    // FIX ME - needs to check for 0 velocity to reset canBeThrown
+    /// <summary>
+    /// this method runs when collision with the 2d collider is detected 
+    /// </summary>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().inRange = true;
+            GetComponentInParent<EnemyDasher>().canBeThrown = true;
+            GetComponentInParent<EnemyDasher>().isHit = true;
+        }
+    }
+    /// <summary>
+    /// this method runs every frame that the collision with the 2d collider is still happening
+    /// </summary>
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().targetPos = collision.transform.position;
+        }
+    }
+    /// <summary>
+    /// this method runs when the collision is no longer happening with the 2d collider
+    /// </summary>
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {           
+            GetComponentInParent<EnemyDasher>().inRange = false;
+        }
+
+    }
+   
+}
+
+
+
