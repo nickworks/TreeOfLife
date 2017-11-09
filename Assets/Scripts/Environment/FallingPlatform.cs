2bc2eb3f45e591913b0dbea0d89604e229cffb92
Cameron Blaine <camerong124@gmail.com>
2017-11-09 14:41:50 -0500

    Falling Platforms Version01
    Platform Added
    Lava Added
    Logic added including commenting of code
    Added and replaced previous falling platform.

--- Assets/Scripts/Environment/FallingPlatform.cs
+++ Assets/Scripts/Environment/FallingPlatform.cs
@@ -5,33 +5,49 @@ using UnityEngine;
 /// <summary>
 /// Falling Platform is a class that creates a falling platform on any object this attached to.
 /// </summary>
-[RequireComponent(typeof(BoxCollider2D))]
-public class FallingPlatform : MonoBehaviour {
-    
+[RequireComponent(typeof(BoxCollider))]
+public class FallingPlatform : MonoBehaviour
+{
+
     /// <summary>
-    /// gets a reference to the rigid body
+    /// Stores a Reference
     /// </summary>
-    private Rigidbody2D rb2d;
+    private Rigidbody rb3d;
+
     /// <summary>
-    /// isfalling the main interaction item here
+    /// Boolean - IsFalling tracks to see if the object is falling 
     /// </summary>
-    public bool isFalling = false;
+    private bool isFalling = false;
+
     /// <summary>
     /// downSpeed is the way that speed is calculated for downfalling objects
     /// </summary>
-    public float downSpeed = 0;
+    private float downSpeed = 0;
 
-    // Use this for initialization
     /// <summary>
-    /// Tag of Player
+    /// Vector3 variable stores original position of object.
+    /// </summary>
+    private Vector3 OriginalPosition;
+
+    /// <summary>
+    ///  Gives Original Position a local position
+    /// </summary>
+    void Start()
+    {
+        OriginalPosition = transform.localPosition;
+    }
+    // Use this for initialization//FIXME - what is this <
+    /// <summary>
     /// Checks to see if the player is tagged. If it is a player the object is set to falling
     /// if not it isfalling is false!
     /// </summary>
     /// <param name="collider"></param>
-    public void OnTriggerEnter2D(Collider2D collider)
+    public void OnTriggerEnter(Collider collider)
     {
+        /// Tag of Player
         if (collider.CompareTag("Player"))
         {
+            print("Hello");
 
             isFalling = true;
         }
@@ -41,26 +57,33 @@ public class FallingPlatform : MonoBehaviour {
         }
     }
     /// <summary>
-    /// Update
     /// Updates and checks to see if the object is falling is checked for true and the transform position is decreased by downspeed by += time.deltatime / 100;
-    /// Once the object hits a Y position it stops and is no longer interactable
     /// TO DO : Add destroy object
     /// </summary>
     void Update()
     {
-        if(isFalling)
+        if (isFalling)
         {
+            print("fuck");
             downSpeed += Time.deltaTime / 100;
             transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
 
         }
-        if (transform.localPosition.y < -40)
+
+        // Once the object hits a Y position it stops and is no longer interactable 
+        if (transform.localPosition.y < -20)
         {
-            isFalling = false;
-            Destroy(this);
-            
+            downSpeed += Time.deltaTime / 15;
+            //Transform Local Position is reset but is increased before hitting the final area.
+            if (transform.localPosition.y < -40)
+            {
+                isFalling = false;
+                transform.position = OriginalPosition;
+                downSpeed = 0;
+            }
+
         }
-        
+
     }
-  
+
 }
---
+++ Assets/Scripts/Environment/LevelRestart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(BoxCollider))]
+///Level Restart
+///
+public class LevelRestart : MonoBehaviour
+{
+    /// <summary>
+    /// Public String of level, where user types in level. //will be changed
+    /// </summary>
+    public string Level;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /// <summary>
+    /// On Collision with Player new scene is loaded
+    /// </summary>
+    /// <param name="collider"></param>
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Level);
+
+        }
+
+    }
+}
