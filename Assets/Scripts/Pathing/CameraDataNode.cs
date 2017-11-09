6d2cf0dbd64c8df6c113f7d9e6f98b4afff21ac6
Jacob Hamberg <halbridious@gmail.com>
2017-11-09 03:20:53 -0500

    Camera Node scene view enhancements
    + Gizmos for the camera nodes
    >each node now displays a camera icon where the camera will be positioned by that node
    >each icon also displays a view frustrum
    
    +Node Handles (incomplete)
    >Custom Editor started that will allow for manipulation of camera node variables with scene view handles
    >handle for camera distance partiallly implimented

--- Assets/Scripts/Pathing/CameraDataNode.cs
+++ Assets/Scripts/Pathing/CameraDataNode.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 /// <summary>
 /// This component stores camera data in a GameObject. It is meant to be added to PathNode objects.
 /// </summary>
@@ -39,7 +40,7 @@ public class CameraDataNode : MonoBehaviour {
         /// <param name="data2"></param>
         /// <param name="p">Percentage (0 - 1). 0 will yield data1; 1 will yield data2.</param>
         /// <returns></returns>
-        static public CameraData Lerp(CameraData data1, CameraData data2, float p)
+        static public CameraData Lerp( CameraData data1, CameraData data2, float p )
         {
             CameraData data3 = new CameraData();
             data3.cameraDistance = Mathf.Lerp(data1.cameraDistance, data2.cameraDistance, p);
@@ -49,11 +50,72 @@ public class CameraDataNode : MonoBehaviour {
             data3.fov = Mathf.Lerp(data1.fov, data2.fov, p);
             return data3;
         }
+
     }
     /// <summary>
     /// Stores camera data
     /// </summary>
     public CameraData cameraData;
 
-    // TODO: build an in-editor preview of the camera
+    /// <summary>
+    /// Draws any gizmos while rendering the scene view.  Projects a representation of the camera.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Quaternion rotation = Quaternion.Euler(cameraData.pitchOffset, cameraData.yawOffset, 0);
+        Vector3 camPos = new Vector3 (0, 0, -cameraData.cameraDistance);
+        camPos = rotation * camPos;//Applies pitch and yaw to the camera's position relative to the node
+        camPos += transform.position;//Apply relative position to world space
+        Gizmos.DrawIcon(camPos, "icon-camera", true);//draw a camera icon
+        Matrix4x4 temp = Gizmos.matrix;//In order to use frustrum drawing, some matrix translations are required
+        Gizmos.matrix = Matrix4x4.TRS(camPos, rotation, Vector3.one);
+        Gizmos.DrawFrustum(Vector3.zero, cameraData.fov, cameraData.cameraDistance * 2, 0, 4/3);
+        Gizmos.matrix = temp;
+    }
+    /// <summary>
+    /// Returns the vector location in worldspace where the camera node will orient the camera.
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetCameraLocation()
+    {
+        Quaternion rotation = GetCameraRotation();
+        Vector3 camPos = new Vector3(0, 0, -cameraData.cameraDistance);
+        camPos = rotation * camPos;//Applies pitch and yaw to the camera's position relative to the node
+        camPos += transform.position;//Apply relative position to world space
+        return camPos;
+    }
+    /// <summary>
+    /// Returns the Quaternion rotation the camera node will orient the camera to.
+    /// </summary>
+    /// <returns></returns>
+    public Quaternion GetCameraRotation()
+    {
+        Quaternion rotation = Quaternion.Euler(cameraData.pitchOffset, cameraData.yawOffset, 0);
+        return rotation;
+    }
+}
+
+[CustomEditor(typeof(CameraDataNode))]
+public class CameraNodeSceneGUI : Editor
+{
+
+    CameraDataNode camNode;
+
+    private void OnEnable()
+    {
+        camNode = (CameraDataNode)target;
+    }
+
+    /// <summary>
+    /// Custom editor events to manipulate things in the scene view
+    /// </summary>
+    private void OnSceneGUI()
+    {
+        //Slider handle for distance
+        camNode.cameraData.cameraDistance = Handles.ScaleValueHandle(camNode.cameraData.cameraDistance, camNode.GetCameraLocation(), camNode.GetCameraRotation(), camNode.cameraData.cameraDistance, Handles.ArrowHandleCap, 5f);
+        if( camNode.cameraData.cameraDistance < 5f ) camNode.cameraData.cameraDistance = 5f;
+        if( camNode.cameraData.cameraDistance > 50f ) camNode.cameraData.cameraDistance = 50f;
+        //Pitch control
+        //Yaw Control
+    }
 }
