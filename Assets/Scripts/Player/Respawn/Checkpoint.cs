ae0a999cdc50e24028a8547d04c5855b6b208a37
Unknown <darkspark52@gmail.com>
2017-11-15 22:46:10 -0500

    Made a new checkpoint and respawn system
    - KillVolume Prefab and Script. Script handles killing the player and setting the, back to the last respawn location
    - SpawnLocation script holds a reference to the PathNode to set the player to
    - SpwanPoint Prefab
    - Checkpoint prefab and script. Handles setting the SpawnPoint for the player to spawn back to.
    - 3dtrack changes are for testing purposes only, doesn't need to be commited in final

---
+++ Assets/Scripts/Player/Respawn/Checkpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Checkpoint class is used to set checkpoint markers for the player to respwan at if they are to die
+/// It sets all the relavent information to the SpwanLocation and SpawnPoint gameobject 
+/// </summary>
+public class Checkpoint : MonoBehaviour {
+
+    /// <summary>
+    /// This holds a reference to the SpwanPoint Gameobject in the scene
+    /// </summary>
+    public GameObject spawnPoint;
+
+    // Use this for initialization
+    void Start () {
+        spawnPoint = GameObject.Find("SpawnPoint");
+	}
+	
+    /// <summary>
+    /// Check if the player has collided with this checkpoint and set the SpwanPoint location to it
+    /// </summary>
+    /// <param name="other">Object that collides with this one (in this case the player object)</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            spawnPoint.GetComponent<SpawnLocation>().spawnNode = other.GetComponent<AlignWithPath>().currentNode;
+            spawnPoint.transform.localPosition = transform.localPosition;
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/Respawn/SpawnLocation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class literally just holds a reference to the PathNode the player should be spawned at
+/// </summary>
+public class SpawnLocation : MonoBehaviour {
+
+    /// <summary>
+    /// Reference to the PathNode the spawnpoint is at
+    /// </summary>
+    public PathNode spawnNode;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
