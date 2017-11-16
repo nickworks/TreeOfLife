b761cfbff1e0e92886a7c682dc593a33740d19eb
Unknown <smithja@ferris.edu>
2017-11-16 11:10:15 -0500

    Got Hive Spawning up and running just need to tweak it a bit
    Got Hive spawning up and running just need to tweak it a bit to get it working
    Right now Hive spawning just shoots out flies, they don't have a death state, flies also need to have logic fixed

---
+++ Assets/Scripts/NPCs/HiveBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiveBehavior : MonoBehaviour {
+
+    /// <summary>
+    /// A reference to the FlyBehavior script that we will use to instantiate the Flies
+    /// </summary>
+    public FlyBehavior fly;
+
+    /// <summary>
+    /// A list of bug game objects
+    /// </summary>
+    List<FlyBehavior> flies = new List<FlyBehavior>();
+
+    /// <summary>
+    /// A reference to the players transform
+    /// </summary>
+    public Transform playerTransform;
+
+    /// <summary>
+    /// A reference to a secondary target for the bugs to follow
+    /// </summary>
+
+    // Use this for initialization
+    void Start () {
+        //TODO: Randomize fly positions
+        //TODO: Make it so flies are able to follow a leader
+        
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+		while(flies.Count < 5)
+        {
+            FlyBehavior newFly = Instantiate(fly, transform.position, transform.rotation);
+            newFly.target = playerTransform;
+            flies.Add(newFly);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        flies[1].isStopped = true;
+    }
+}
