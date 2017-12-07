c64449c38fe2165db857fc362f34c6f3ee0729af
Cameron Blaine <camerong124@gmail.com>
2017-12-07 10:37:30 -0500

    Version 13
    Updated Options Menu and Start Screen
    Added : Graphicsettings, quality settings, editorbuildsettings, and
    allow for player to choose fullscreen mode, volume control, and
    resolutions

---
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    /// <summary>
+    /// This checks to see what the resolutions are and automatically retrieves a list of resolutions
+    /// This is for the dropdown menu for the resolutions
+    /// </summary>
+    private void Start()
+    {
+        DropdownMenuResolutions();
+    }
+
+    private void DropdownMenuResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void Brightness(float volume)
+    {
+
+    }
+    public void Contrast(float volume)
+    {
+
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
