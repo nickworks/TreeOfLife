c5ef3163966685303a66f206dd2b9ba6f82505e0
Cameron Blaine <camerong124@gmail.com>
2017-12-07 13:07:54 -0500

    Version 13.5 Options Menu
    Added Brightness and contrast - currently not working
    
    Added Comments to the code

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -6,11 +6,32 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
 
+    /// <summary>
+    /// Audiomixer accesses master volume
+    /// </summary>
     public AudioMixer audioMixer;
 
+
+    /// <summary>
+    /// Accesses dropdown resolutions
+    /// </summary>
     public Dropdown resolutionDropdown;
 
+    /// <summary>
+    /// Array of Resolutions
+    /// </summary>
     Resolution[] resolutions;
+
+    /// <summary>
+    /// does not work 
+    /// </summary>
+    ///  public float GammaCorrection;
+
+    /// <summary>
+    /// does not work 
+    /// </summary>
+    public Rect SliderLocation;
+
 
     /// <summary>
     /// This checks to see what the resolutions are and automatically retrieves a list of resolutions
@@ -18,9 +39,13 @@ public class OptionsMenu : MonoBehaviour {
     /// </summary>
     private void Start()
     {
+     //?   RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
         DropdownMenuResolutions();
     }
-
+    /// <summary>
+    /// This is the resolutions tab for the game.
+    /// This automatically updates the resolution tabs for use in the options menu
+    /// </summary>
     private void DropdownMenuResolutions()
     {
         resolutions = Screen.resolutions;
@@ -46,29 +71,55 @@ public class OptionsMenu : MonoBehaviour {
         resolutionDropdown.RefreshShownValue();
     }
 
+    /// <summary>
+    /// Sets the Volume on a slider
+    /// </summary>
+    /// <param name="volume"></param>
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    /// <summary>
+    /// Controls Brightness // still being written
+    /// </summary>
+    /// <param name="volume"></param>
     public void Brightness(float volume)
     {
-
+     //   GammaCorrection = GUI.HorizontalSlider(SliderLocation, GammaCorrection, 0, 1.0f);
     }
+
+    /// <summary>
+    /// Controls contrast // still be written
+    /// </summary>
+    /// <param name="volume"></param>
     public void Contrast(float volume)
     {
 
     }
 
+    /// <summary>
+    /// Set the Quality of the game
+    /// </summary>
+    /// <param name="qualityIndex"></param>
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
+    /// <summary>
+    /// Set the game to fullscreen
+    /// </summary>
+    /// <param name="isFullScreen"></param>
     public void setFullscreen (bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
     }
 
+    /// <summary>
+    /// Sets Resolution of computer 
+    /// </summary>
+    /// <param name="resolutionIndex"></param>
     public void SetResolution (int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
